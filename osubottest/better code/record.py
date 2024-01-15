ab119113e84b962a01051a3d653c4e87c961786b
Stromy08 <fajnygoscsteve@gmail.com>
2024-01-15 19:12:11 +0000

    cleanup

--- osubottest/better code/record.py
+++ osubottest/better code/record.py
@@ -7,7 +7,7 @@ import numpy as np
 
 screen_size = (1920, 1080)  # Change this to match your screen resolution
 fps = 30
-output_filename = "screen_record.avi"
+output_filename = input("enter filename: \n") + ".avi"
 
 
 
@@ -15,11 +15,8 @@ output_filename = "screen_record.avi"
 fourcc = cv2.VideoWriter_fourcc(*"XVID")
 out = cv2.VideoWriter(output_filename, fourcc, fps, screen_size)
 
-
-
-
-while True:
-    # Capture screen content
+def record_screen():
+        # Capture screen content
     frame = pyautogui.screenshot()
     frame = np.array(frame)
 
@@ -29,9 +26,17 @@ while True:
     # Write the frame to the video file
     out.write(frame)
 
-    # Stop recording when the user presses the 'q' key
-    if cv2.waitKey(1) == ord("q"):
-        break
+def record_inputs():
+    print("oky")
+
+
+while True:
+
+    #run code from above
+    record_screen()
+    # Stop recording when the user presses the 'q' key  #--- DOESN'T WORK! ---#
+    if cv2.waitKey(1) == ord("q"):                      #--- DOESN'T WORK! ---#
+        break                                           #--- DOESN'T WORK! ---#
 
 # Release the VideoWriter and close the OpenCV windows
 out.release()
